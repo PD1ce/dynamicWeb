0ef30a73cd0f0b21373c38ca526aade80108ae60
PD1ce <padeisinger@gmail.com>
2015-03-12 13:15:09 -0500

    Starting home page

---
+++ js/index.js
@@ -0,0 +1,31 @@
+/*
+var headerNormal = true;
+
+$(document).ready(function() {
+	//$('div#header').remove()
+	//console.log("Hello");
+	$('div#header').mouseover(function() {
+		console.log("header mouseover")
+		$('div#header').css('background-color', '#FF00FF');
+		$('div#header').css('width', '100%')
+		$('div#header').css('height', '50px')
+		$('div#header').css('line-height', '50px')
+		$('div#header').css('left', '0%')
+
+		
+	})
+	$('div#header').mouseleave(function() {
+		console.log("header mouseleave")
+		$('div#header').css('background-color', '#FF0000');
+		$('div#header').css('width', '50%')
+		$('div#header').css('height', '20px')
+		$('div#header').css('line-height', '20px')
+		$('div#header').css('left', '25%')
+	})
+
+	$('div#header').click(function() {
+		console.log("header click")
+	})
+	
+})
+*/
